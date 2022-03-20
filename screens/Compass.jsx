464ba8b8e07ce713980c2ee1a9b7ed481d99d101
Michal Stehlík <michal.stehlik@pslib.cz>
2022-03-20 20:28:04 +0100

    init

---
+++ screens/Compass.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+export const Compass = props => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Compass</Text>
+        </View>
+    );
+}
+
+export default Compass;
